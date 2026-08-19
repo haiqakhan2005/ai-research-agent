67ab374b28d78cfc501f01658c66e3c749f79cc4
haiqakhan2005 <haiqakhan2005@gmail.com>
2026-08-19 08:26:22 +0500

    Configure Vercel deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:8000/:path*",
+        },
+      ];
+    }
+
+    return [];
+  },
 };
 
 export default nextConfig;
